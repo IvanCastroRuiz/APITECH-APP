c2fd2dfaae3bbf9ad8a0c6a9cdf559b6453493d9
IvanCastroRuiz <ivan.castro@devitech.com.co>
2025-03-11 10:39:19 -0500

    hotfix: refactorizacion rama main

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import AuthLayout from './layout/AuthLayout';
-// te hacia falta en el import AuthLayout
-function App() {
-    return (_jsx(BrowserRouter, { children: _jsx(Routes, { children: _jsx(Route, { path: "/", element: _jsx(AuthLayout, {}) }) }) }));
-}
-export default App;
